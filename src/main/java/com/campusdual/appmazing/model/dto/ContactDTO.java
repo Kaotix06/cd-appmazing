185edf5ec2d33f165de53847e95fc7997a2687d2
Néstor Riveiro Martínez <nestor_rm06@hotmail.com>
2023-09-26 12:24:38 +0200

    Add plugins for launch DB, \renamed properties to *yml \Add table PRODUCTS \Add table CONTACTS \Add java Product.java \Add java Contact.java \Add java ProductDao \Add java ContactDao \Add java ProductDTO \Add java ContactDTO

---
+++ src/main/java/com/campusdual/appmazing/model/dto/ContactDTO.java
@@ -0,0 +1,64 @@
+package com.campusdual.appmazing.model.dto;
+
+public class ContactDTO {
+
+    private int id;
+
+    private String name;
+
+    private String middlename;
+
+    private String lastname;
+
+    private double phone;
+
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public double getPhone() {
+        return phone;
+    }
+
+    public void setPhone(double phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
