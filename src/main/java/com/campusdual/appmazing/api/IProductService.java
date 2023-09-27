c2c950adbc0cb3dc30fdda26fb18a51293b81236
Néstor Riveiro Martínez <nestor_rm06@hotmail.com>
2023-09-27 11:48:46 +0200

    Add plugins for launch DB, \renamed properties to *yml \Add table PRODUCTS \Add table CONTACTS \Add java Product.java \Add java Contact.java \Add java ProductDao \Add java ContactDao \Add java ProductDTO \Add java ContactDTO \Add mappers \Add Product mappers \Add Contact mappers \Add IProductService api \Add IContactService api \Add ProductService service

---
+++ src/main/java/com/campusdual/appmazing/api/IProductService.java
@@ -0,0 +1,14 @@
+package com.campusdual.appmazing.api;
+
+import com.campusdual.appmazing.model.dto.ProductDTO;
+
+import java.util.List;
+
+public interface IProductService {
+    ProductDTO queryProduct(ProductDTO productDTO);
+    List<ProductDTO> queryAllProducts();
+
+    int insertProduct(ProductDTO productDTO);
+    int updateProduct(ProductDTO productDTO);
+    int deleteProduct(ProductDTO productDTO);
+}
