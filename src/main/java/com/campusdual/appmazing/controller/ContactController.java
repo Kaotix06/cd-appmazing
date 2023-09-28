0ed64991a5a21b6a8cd61489107033cc17b81012
Néstor Riveiro Martínez <nestor_rm06@hotmail.com>
2023-09-28 10:36:48 +0200

    Add plugins for launch DB, \renamed properties to *yml \Add table PRODUCTS \Add table CONTACTS \Add java Product.java \Add java Contact.java \Add java ProductDao \Add java ContactDao \Add java ProductDTO \Add java ContactDTO \Add mappers \Add Product mappers \Add Contact mappers \Add IProductService api \Add IContactService api \Add ProductService service \Add ContactService service

--- src/main/java/com/campusdual/appmazing/controller/ContactController.java
+++ src/main/java/com/campusdual/appmazing/controller/ContactController.java
@@ -1,20 +1,34 @@
 package com.campusdual.appmazing.controller;
 
+import com.campusdual.appmazing.api.IContactService;
+import com.campusdual.appmazing.model.dto.ContactDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
+
+    @Autowired
+    IContactService contactService;
+
     @GetMapping
     public String testController() {
         return "Contact controller works!";
     }
+
     @PostMapping
     public String testController(@RequestBody String name) {
         return "Contact controller works, " + name + "!";
     }
+
     @GetMapping(value = "/testMethod")
     public String testControllerMethod(){
         return "Contact controller method works!";
+    }
+
+    @PostMapping(value = "/get")
+    public ContactDTO queryContact(@RequestBody ContactDTO contact){
+        return this.contactService.queryContact(contact);
     }
 }
