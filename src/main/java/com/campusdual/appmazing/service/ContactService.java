417cfc9e74811a5060780a472c6770f78240251b
Néstor Riveiro Martínez <nestor_rm06@hotmail.com>
2023-09-29 10:05:29 +0200

    Add plugins for launch DB, \renamed properties to *yml \Add table PRODUCTS \Add table CONTACTS \Add java Product.java \Add java Contact.java \Add java ProductDao \Add java ContactDao \Add java ProductDTO \Add java ContactDTO \Add mappers \Add Product mappers \Add Contact mappers \Add IProductService api \Add IContactService api \Add ProductService service \Add ContactService service \Add ProductController controller \Add ContactController controller \Add Postman Collection \Add Category \Add CategoryDao \Add CategoryDTO \Add CategoryMapper \Add ICategoryService api \Add CategoryService

--- src/main/java/com/campusdual/appmazing/service/ContactService.java
+++ src/main/java/com/campusdual/appmazing/service/ContactService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("ContactServise")
+@Service("ContactService")
 @Lazy
 public class ContactService implements IContactService {
 
